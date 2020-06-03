92efd6f1cf07a6123d8bd6136134da5953a6ba33
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-06-02 22:10:53 -0300

    NLW - create-point-api localidades-fix

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,4 +1,4 @@
-function populateUF(){
+function getUFs(){
     const ufSelect = document.querySelector("select[name=uf]")
     
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -14,11 +14,34 @@ function populateUF(){
     } )
 }
 
-populateUF()
+getUFs()
+
+function getCities(){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    
+    fetch(url)
+    .then( (res)=>{ return res.json()}) //res => res.json()
+    .then( cities => {
+
+        for(const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+}
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change" ,() => {
-        console.log('mudei')
-    })
+    .addEventListener("change",getCities)
 
