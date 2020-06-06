83a9f811106597d9780de7be0b0f5c6b4b274081
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-06-05 22:11:25 -0300

    NLW - ecoleta

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 const express = require("express")
 const server = express()
 
-
+const db = require("./database/db.js")
 //configurar pasta publica
 
 server.use(express.static("public"))
 
+//habilitar red.body
+
+server.use(express.urlencoded({ extended: true }))
+
 //template engine
 
-const  nunjucks = require("nunjucks")
+const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
+   express: server,
+   noCache: true
 })
 
 //caminhos
@@ -19,20 +23,86 @@ nunjucks.configure("src/views", {
 //res == resposta
 
 
-server.get("/", (req,res)=>{
+server.get("/", (req, res) => {
 
-   return res.render("index.html", {title:"Um titulo"})
+   return res.render("index.html", { title: "Um titulo" })
 
 })
-server.get("/create-point", (req,res)=>{
+
+server.get("/create-point", (req, res) => {
 
    return res.render("create-point.html")
 
+   req.query
+
 })
 
-server.get("/search-results", (req,res)=>{
+server.post("/savepoint", (req, res) => {
 
-   return res.render("search-results.html")
+
+   const query = `
+        INSERT INTO places(
+            image,
+            name, 
+            address, 
+            address2, 
+            state, 
+            city, 
+            items
+        ) VALUES(
+            ?, ?, ?, ?, ?, ?, ?
+        );
+`
+   const values = [
+      req.body.image,
+      req.body.name,
+      req.body.address,
+      req.body.address2,
+      req.body.state,
+      req.body.city,
+      req.body.items
+   ]
+
+   function afterInsertData(err) {
+      if (err) {
+         console.log(err)
+         return res.send("Erro de cadastro!")
+      }
+
+      console.log('cadastrado com sucesso!')
+      console.log(this)
+
+      return res.render("create-point.html" ,{saved:true})
+   }
+   db.run(query, values, afterInsertData);
+
+})
+
+server.get("/search-results", (req, res) => {
+
+   const search = req.query.search
+
+   // if(search == ""){
+   //  return res.render("search-results.html", { total: 0})
+
+   // }
+
+   db.all(` SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+      if (err) {
+         return console.log(err)
+      }
+
+      console.log("Aqui estão os seus registros")
+      console.log(rows)
+
+      const total = rows.length
+
+      return res.render("search-results.html", { places: rows, total: total })
+      
+   })
+
+
+
 
 })
 
